268701a95ef333e03c22713f75a785ebc1752539
abeidahmed <abeidahmed92@gmail.com>
2021-03-03 20:19:45 +0530

    Add contact section layout and styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,7 +8,22 @@ function createContact() {
   heading.textContent = 'Contact information';
   heading.classList.add('main-heading');
 
+  const addressWrapper = document.createElement('div');
+  addressWrapper.classList.add('address');
+
+  const address = document.createElement('p');
+  address.classList.add('main-description');
+  address.textContent = '$ Lowly Road, Missi, New Terkic';
+
+  const phoneNumber = document.createElement('p');
+  phoneNumber.classList.add('main-description');
+  phoneNumber.textContent = '# 98-636874521';
+
+  addressWrapper.append(address);
+  addressWrapper.append(phoneNumber);
+
   wrapper.append(heading);
+  wrapper.append(addressWrapper);
 
   return wrapper;
 }
