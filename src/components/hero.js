56dd6e4525d68bb302cbd7177e08bfbd8b97f801
abeidahmed <abeidahmed92@gmail.com>
2021-03-03 16:08:21 +0530

    Style hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,19 +3,24 @@ const DESCRIPTION = 'All the best foods that you can taste in town.';
 
 function createHome() {
   const wrapper = document.createElement('section');
+  wrapper.classList.add('hero-wrapper');
+
   const headline = document.createElement('h1');
+  headline.classList.add('hero-headline');
+
   const tagLine = document.createElement('p');
+  tagLine.classList.add('hero-tagline');
 
   headline.innerText = HEADLINE;
   tagLine.innerText = DESCRIPTION;
 
-  wrapper.appendChild(headline);
-  wrapper.appendChild(tagLine);
+  wrapper.append(headline);
+  wrapper.append(tagLine);
 
   return wrapper;
 }
 
 export default function hero() {
   const content = document.getElementById('content');
-  content.appendChild(createHome());
+  content.append(createHome());
 }
